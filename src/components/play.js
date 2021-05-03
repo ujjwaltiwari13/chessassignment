288cb694c532e2e6739bb230cad356db1c784649
Ujjwal Tiwari <43015170+ujjwaltiwari13@users.noreply.github.com>
2021-05-03 20:20:26 +0530

    Add files via upload

---
+++ src/components/play.js
@@ -0,0 +1,165 @@
+import React from 'react';
+
+import '../index.css';
+import Board from './chesspage.js';
+import King from '../pieces/king'
+import FallenSoldierBlock from './eliminated.js';
+import initialiseChessBoard from '../helpers/board-initialiser.js';
+
+export default class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: initialiseChessBoard(),
+      whiteFallenSoldiers: [],
+      blackFallenSoldiers: [],
+      player: 1,
+      sourceSelection: -1,
+      status: '',
+      turn: 'white'
+    }
+  }
+
+ 
+
+  getKingPosition(squares, player) {
+    return squares.reduce((acc, curr, i) =>
+      acc || //King may be only one, if we had found it, returned his position
+      ((curr //current squre mustn't be a null
+        && (curr.getPlayer() === player)) //we are looking for aspecial king 
+        && (curr instanceof King)
+        && i), // returned position if all conditions are completed
+      null)
+  }
+
+  handleClick(i) {
+    const squares = [...this.state.squares];
+
+    if (this.state.sourceSelection === -1) {
+      if (!squares[i] || squares[i].player !== this.state.player) {
+        if (squares[i]) {
+          squares[i].style = { ...squares[i].style, backgroundColor: "" };
+        }
+      }
+      else {
+        squares[i].style = { ...squares[i].style, backgroundColor: "RGB(111,143,114)" }; // Emerald from http://omgchess.blogspot.com/2015/09/chess-board-color-schemes.html
+        this.setState({
+          sourceSelection: i
+        })
+      }
+      return
+    }
+
+    squares[this.state.sourceSelection].style = { ...squares[this.state.sourceSelection].style, backgroundColor: "" };
+
+    if (squares[i] && squares[i].player === this.state.player) {
+      this.setState({
+        status: "Can't Move",
+        sourceSelection: -1,
+      });
+    }
+    else {
+
+      const whiteFallenSoldiers = [];
+      const blackFallenSoldiers = [];
+      const isDestEnemyOccupied = Boolean(squares[i]);
+      const desiremove = squares[this.state.sourceSelection].desiremove(this.state.sourceSelection, i, isDestEnemyOccupied);
+
+      if (desiremove) {
+        if (squares[i] !== null) {
+          if (squares[i].player === 1) {
+            whiteFallenSoldiers.push(squares[i]);
+          }
+          else {
+            blackFallenSoldiers.push(squares[i]);
+          }
+        }
+
+        squares[i] = squares[this.state.sourceSelection];
+        squares[this.state.sourceSelection] = null;
+
+        const isCheckMe = this.isCheckForPlayer(squares, this.state.player)
+
+        if (isCheckMe) {
+          this.setState(oldState => ({
+            status: "Can't Move",
+            sourceSelection: -1,
+          }))
+        } else {
+          let player = this.state.player === 1 ? 2 : 1;
+          let turn = this.state.turn === 'white' ? 'black' : 'white';
+
+          this.setState(oldState => ({
+            sourceSelection: -1,
+            squares,
+            whiteFallenSoldiers: [...oldState.whiteFallenSoldiers, ...whiteFallenSoldiers],
+            blackFallenSoldiers: [...oldState.blackFallenSoldiers, ...blackFallenSoldiers],
+            player,
+            status: '',
+            turn
+          }));
+        }
+      }
+      else {
+        this.setState({
+          status: "Can't Move",
+          sourceSelection: -1,
+        });
+      }
+    }
+  }
+
+  isCheckForPlayer(squares, player) {
+    const opponent = player === 1 ? 2 : 1
+    const playersKingPosition = this.getKingPosition(squares, player)
+    const canPieceKillPlayersKing = (piece, i) => piece.desiremove(playersKingPosition, i, squares)
+    return squares.reduce((acc, curr, idx) =>
+      acc ||
+      (curr &&
+        (curr.getPlayer() === opponent) &&
+        canPieceKillPlayersKing(curr, idx)
+        && true),
+      false)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={this.state.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <h3>Player</h3>
+            <div id="player-turn-box" style={{ backgroundColor: this.state.turn }}>
+
+            </div>
+            <div className="game-status">{this.state.status}</div>
+
+            <div className="eliminated">
+
+              {<FallenSoldierBlock
+                whiteFallenSoldiers={this.state.whiteFallenSoldiers}
+                blackFallenSoldiers={this.state.blackFallenSoldiers}
+              />
+              }
+            </div>
+
+          </div>
+        </div>
+        
+        <ul>
+          <li><a href="https://www.chesscentral.com/pages/first-moves-in-chess/the-rules-of-chess.html" target="_blank">READ RULES</a> </li>
+        </ul>
+        
+      </div>
+
+
+    );
+  }
+}
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import Game from './components/play.js'
+
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
